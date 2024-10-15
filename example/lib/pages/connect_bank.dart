362f9e5d4e5a8f14b6220aef8004a6b6e3cc8c96
Kingkor Roy Tirtho <krtirtho@maxint.com>
2024-10-15 13:14:16 +0600

    feat: use flutter_inappwebview for Windows and macOS

--- example/lib/pages/connect_bank.dart
+++ example/lib/pages/connect_bank.dart
@@ -7,6 +7,9 @@ class ConnectBankPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Connect Bank"),
+      ),
       body: TellerConnect(
         onExit: () {
           // pop the page maybe?
